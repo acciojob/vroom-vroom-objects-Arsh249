02e86e511c1d84b5c581ef66e8a613e7e0df05e1
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-24 16:19:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
 	this.topSpeed = topSpeed
 }
-Object.setPrototypeOf(SportsCar,Car);
+Object.setPrototypeOf(SportsCar.prototype,Car.prototype);
 
 SportsCar.prototype.getTopSpeed = ()=>{
 	return topSpeed;
