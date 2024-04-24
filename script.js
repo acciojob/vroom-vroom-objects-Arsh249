95badb060d56c53b48bd6dd14900ae5b03b30702
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-04-24 16:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = () =>{
+	return (make, model);
+}
+
+function SportsCar(make, model, topSpeed) {
+	Car.call(this,make,model);
+	this.topSpeed = topSpeed
+}
+Object.setPrototypeOf(SportsCar,Car);
+
+SportsCar.prototype.getTopSpeed = ()=>{
+	return topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
